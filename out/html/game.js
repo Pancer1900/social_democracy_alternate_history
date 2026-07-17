b8f03d14e9932fb927543a39cff62ba801451a63
Pancer1900 <krzysztof.julian.klosowski@gmail.com>
2026-07-17 19:54:05 +0200

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -189,6 +189,14 @@
       $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
   };
 
+    window.updateSidebarRight = function() {
+    $('#qualities_right').empty();
+    var scene = dendryUI.game.scenes[window.statusTabRight];
+    dendryUI.dendryEngine._runActions(scene.onArrival);
+    var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
+    $('#qualities_right').append(dendryUI.contentToHTML.convert(displayContent));
+};
+
   window.changeTab = function(newTab, tabId) {
       if (tabId == 'poll_tab' && dendryUI.dendryEngine.state.qualities.historical_mode) {
           window.alert('Polls are not available in historical mode.');
